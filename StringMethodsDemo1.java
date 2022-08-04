a1c425d781b38dcbd14de53cfb142540e67532a4
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-08-04 08:40:08 +0530

    String class methods

---
+++ StringMethodsDemo1.java
@@ -0,0 +1,97 @@
+/*
+    String class important methods:
+	--------------------------------
+
+    1.  public char charAt(int  index);
+	2.  public String concat(String  literal);
+	3.  public boolean equals(String value);
+	4.  public boolean equalsIgnoreCase(String value);
+	5.  public String substring(int  beginindex)
+	6.  public String substring(int  beginindex,int  endIndex)
+
+	. public  int  length();
+
+	
+*/
+class StringMethodsDemo1 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		String  s1=new String("java");
+
+		System.out.println(s1);
+
+		int  l=s1.length();
+
+		System.out.println("Lenth of string  "+s1 +" is "+l);
+
+
+		System.out.println("Lenth of string  "+s1 +" is "+s1.length());
+
+
+
+		
+
+		String  s1=new String("bytecode");
+
+		char   c=s1.charAt(4);
+
+		System.out.println(c);
+
+		System.out.println(s1.charAt(4));
+
+
+		
+		 String  s1=new String("java");
+
+		 String s2 =s1.concat("java");
+
+		 System.out.println(s2);//
+
+		 System.out.println(s2.length());
+
+
+		 
+
+
+		 
+		 String  s1=new String("java");
+          
+		 // "java".equals(s1);
+
+		 boolean b1=s1.equals("Java");//password  ==>false 
+
+		 boolean b2=s1.equalsIgnoreCase("JAVA");//userid   =>true
+
+
+		 System.out.println(b1);//
+
+		 System.out.println(b2);//
+
+
+		 
+
+
+		 String  s1=new String("javaatbytecode");
+
+		 String  s2= s1.substring(6);
+
+		 System.out.println(s2);
+
+
+		
+      
+	     String  s1=new String("javaatbytecode");
+
+		 String  s2= s1.substring(4,6);
+
+		 System.out.println(s2);
+
+		  */
+
+
+
+
+	}
+}
